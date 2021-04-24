ee87b3175cea5f4aa57ba65fd87455da507db175
Leon Vreling <l.m.vreling@student.tue.nl>
2021-04-24 16:10:18 +0200

    Add archive draft

--- app/supremum/routes.py
+++ app/supremum/routes.py
@@ -1,7 +1,39 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, url_for
 from . import supremum_bp as supremum
 
 
 @supremum.route('')
 def supremum_overview():
-    return "This is the temporary supremum archive"
+    temp_editions = [
+        {
+            'title': 'The Wind Edition',
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'name': 'Supremum 53.1'
+        },
+        {
+            'title': 'The Earth Edition',
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'name': 'Supremum 53.0'
+        },
+        {
+            'title': 'The Default Edition',
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'name': 'Supremum 52.2'
+        },
+        {
+            'title': 'The Miracle Edition',
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'name': 'Supremum 52.1'
+        },
+        {
+            'title': 'The Disaster Edition',
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'name': 'Supremum 52.0'
+        }
+    ]
+    return render_template('archive.html', editions=temp_editions)
