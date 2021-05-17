8b2466d63b856eb90c24302a90d70d5992acee04
Takke <e.c.takke@student.tue.nl>
2021-05-17 23:06:50 +0200

    Update supremum archive + small bugfix

--- app/supremum/routes.py
+++ app/supremum/routes.py
@@ -4,36 +4,45 @@ from . import supremum_bp as supremum
 
 @supremum.route('')
 def supremum_overview():
-    temp_editions = [
-        {
-            'title': 'The Wind Edition',
-            'img_url' : url_for("home.static", filename="latest_edition.png"),
-            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
-            'name': 'Supremum 53.1'
-        },
-        {
-            'title': 'The Earth Edition',
-            'img_url' : url_for("home.static", filename="latest_edition.png"),
-            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
-            'name': 'Supremum 53.0'
-        },
-        {
-            'title': 'The Default Edition',
-            'img_url' : url_for("home.static", filename="latest_edition.png"),
-            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
-            'name': 'Supremum 52.2'
-        },
-        {
-            'title': 'The Miracle Edition',
-            'img_url' : url_for("home.static", filename="latest_edition.png"),
-            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
-            'name': 'Supremum 52.1'
-        },
-        {
-            'title': 'The Disaster Edition',
-            'img_url' : url_for("home.static", filename="latest_edition.png"),
-            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
-            'name': 'Supremum 52.0'
+    volumes = {
+            53: [
+                {
+                    'title': 'The Wind Edition',
+                    'img_url' : url_for("home.static", filename="latest_edition.png"),
+                    'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+                    'name': 'Supremum 53.1',
+                    'edition': '1'
+                },
+                {
+                    'title': 'The Earth Edition',
+                    'img_url' : url_for("home.static", filename="latest_edition.png"),
+                    'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+                    'name': 'Supremum 53.0',
+                    'edition': '0'
+                },
+            ],
+            52: [
+                {
+                    'title': 'The Default Edition',
+                    'img_url' : url_for("home.static", filename="latest_edition.png"),
+                    'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+                    'name': 'Supremum 52.2',
+                    'edition': '2'
+                },
+                {
+                    'title': 'The Miracle Edition',
+                    'img_url' : url_for("home.static", filename="latest_edition.png"),
+                    'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+                    'name': 'Supremum 52.1',
+                    'edition': '1'
+                },
+                {
+                    'title': 'The Disaster Edition',
+                    'img_url' : url_for("home.static", filename="latest_edition.png"),
+                    'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+                    'name': 'Supremum 52.0',
+                    'edition': '0'
+                }        
+            ]
         }
-    ]
-    return render_template('archive.html', editions=temp_editions)
+    return render_template('archive.html', volumes=volumes)
