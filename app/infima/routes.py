4324bf6c36f438a33a69d9450bfa1fcabad4d4c3
Takke <e.c.takke@student.tue.nl>
2021-06-21 22:36:53 +0200

    Add basic front-end support for infima

--- app/infima/routes.py
+++ app/infima/routes.py
@@ -1,13 +1,70 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, abort
 from . import infima_bp as infima
 
 from app.infima.forms import SubmitForm
 from app.infima.models import Infimum
 
+import re
+
 @infima.route('/')
 def infima_overview():
-    return render_template('infima.html'), 200
+    temp_editions = [
+        {
+            "volume_nr": 53,
+            "edition_nr": 0,
+            "theme": "The Earth Edition"
+        },
+        {
+            "volume_nr": 53,
+            "edition_nr": 1,
+            "theme": "The Wind Edition"
+        }
+    ]
+    return render_template('infima_overview.html', editions=temp_editions), 200
 
+
+@infima.route('/<string:edition>')
+def infima_for_edition(edition):
+    try:
+        res = re.match('(?P<volume_nr>\d*).(?P<edition_nr>\d)\Z', edition)
+        volume_nr, edition_nr = res.group('volume_nr'), res.group('edition_nr')
+    except Exception as e:
+        return abort(404)
+    
+    # TODO: add check that edition is available !
+    
+    # try:
+    #     infima = Infimum.query.filter_by(
+    #         volume_nr=volume_nr, 
+    #         edition_nr=edition_nr,
+    #         rejected=0
+    #     )
+    # except Exception as e:
+    #     return abort(500)
+    
+    test_infima = [
+        {
+            "id": 1,
+            "supremum_id": 1,
+            "content": "Erik en Leon zijn de supremum website aan het maken",
+            "submission_date": "2021-05-27",
+            "rejected": 0
+        },
+        {
+            "id": 2,
+            "supremum_id": 2,
+            "content": "Erik en Leon zijn nog meer supremum website aan het maken",
+            "submission_date": "2021-05-27",
+            "rejected": 0
+        }
+    ]
+    
+    return render_template(
+        'infima_edition.html', 
+        infima=test_infima,
+        volume_nr=volume_nr,
+        edition_nr=edition_nr
+    ), 200
 
 @infima.route('/submit', methods=['GET', 'POST'])
 def submit():
