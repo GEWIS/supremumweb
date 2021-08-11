c1f5a02ffc04715602e41ff2d649540ad5e002ed
Takke <e.c.takke@student.tue.nl>
2021-08-11 14:49:37 +0200

    feat: basics to create and update supremum edition

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -1,7 +1,8 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from flask_login import login_required
 
 from app.admin import admin_bp as admin
+from app.admin.forms import SupremumForm
 
 @admin.route('/')
 # @login_required
@@ -43,8 +44,38 @@ def index():
             'id': 0
         }
     ]
+    # TODO: retrieve from database.
     return render_template('index.html', editions=temp_editions), 200
 
-@admin.route('/supremum/edit/<int:sid>')
+@admin.route('/supremum/new', methods=["GET", "POST"])
+def new_supremum():
+    form = SupremumForm()
+    if form.validate_on_submit():
+        # Add supremum to database
+        # TODO
+        
+        # Return to admin panel
+        return redirect(url_for("admin.index"))
+    return render_template("edit_supremum_form.html", form=form), 200
+
+@admin.route('/supremum/edit/<int:sid>', methods=["GET", "POST"])
 def edit_supremum(sid: int):
-    return "Not yet implemented", 404
+    # Retrieve the supremum with sid
+    temp_supremum = {
+            'theme': 'The Disaster Edition',
+            'volume_nr': 52,
+            'edition_nr': 0,
+            'img_url' : url_for("home.static", filename="latest_edition.png"),
+            'pdf_url' : url_for("home.static", filename="latest_supremum.pdf"),
+            'id': 0,
+            'published': True
+    }
+    # TODO: retrieve from the database
+    
+    form = SupremumForm(supremum=temp_supremum)
+    if form.validate_on_submit():
+        # Update supremum in database...
+        
+        # Return to admin panel
+        return redirect(url_for("admin.index"))
+    return render_template("edit_supremum_form.html", form=form), 200
