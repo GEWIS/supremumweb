431687e34f5af67a7a47e6bc93bb5bd4b1de7b90
Erikct98 <e.c.takke@student.tue.nl>
2021-08-28 14:56:33 +0200

    chore: clean up login remainders

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,9 +1,9 @@
 from flask import Blueprint
 
 auth_bp = Blueprint(
-    "auth", __name__, 
-    static_folder='static',
-    template_folder='templates'
+    "auth", __name__,
+    static_folder=None,
+    template_folder=None
 )
 
 from . import routes
