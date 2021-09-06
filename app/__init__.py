385b79e7464959856403bbafe0caa2d9070e4a08
Erikct98 <e.c.takke@student.tue.nl>
2021-09-06 09:51:02 +0200

    feat: attempt to incorporate CORS support

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from app.admin import admin_bp
 from app.api import api_bp
 from app.auth import auth_bp
 from app.database import db
-from app.extensions import cache
-from app.extensions import lm
+from app.extensions import cache, cors, lm
 from app.home import home_bp
 from app.tools import http_code
 
@@ -25,8 +24,9 @@ def create_app(config=config.base_config):
 
 def register_extensions(app):
     db.init_app(app)
-    lm.init_app(app)
     cache.init_app(app)
+    cors.init_app(app)
+    lm.init_app(app)
     lm.login_view = 'auth.login'  # TODO: give this a better spot...
 
 
--- app/extensions.py
+++ app/extensions.py
@@ -1,7 +1,13 @@
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_caching import Cache
+from flask_cors import CORS
 
 fbcrypt = Bcrypt()
 lm = LoginManager()
 cache = Cache(config={'CACHE_TYPE': "simple"})
+cors = CORS(
+    origins=['gewis.nl', '*.gewis.nl', 'win.tue.nl',
+             '*.win.tue.nl', 'localhost:8080'],
+    resources=[r'/api/*']
+)
